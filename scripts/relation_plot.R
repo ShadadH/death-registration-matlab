fbd9dd2ffa61a41417a181771b298c2cfe1ea53e
ShadadH <mfh9822@nyu.edu>
2025-03-15 12:25:11 +0400

    Updated mosiac plot and pacman packages

--- scripts/relation_plot.R
+++ scripts/relation_plot.R
@@ -10,19 +10,27 @@ data <- readRDS(here::here("data", "cleaned_data.rds"))
 data_filtered <- data %>%
   filter(relation_group != "Unknown")
 
+data_filtered$relation_group <- droplevels(data_filtered$relation_group)
+
 # Relationship Mosaic Plot
-ggplot(data_filtered) +
-  geom_mosaic(aes(weight = 1, x = product(age_group_broad), fill = relation_group)) +
+ggplot(data_filtered) +  
+  geom_mosaic(aes(weight = 1, x = product(age_group_broad), fill = relation_group),
+              offset = 0) +  
+  scale_y_continuous(
+    breaks = seq(0, 1, by = 0.25),
+    labels = c("0%", "25%", "50%", "75%", "100%")
+  ) +
   labs(
     title = "Relationship Between Deceased and Registrar by Age Group",
     x = "\nAge Group of Deceased",
-    y = "Proportion",
+    y = "Proportion (%)",
     fill = "Relationship Type"
   ) +
   theme_minimal() +
   theme(
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
+    axis.text.y = element_text(size = 10),
+    axis.ticks.y = element_line(),
+    axis.ticks.x = element_line(),
     legend.title = element_text(size = 12),
     legend.text = element_text(size = 10),
     axis.text.x = element_text(angle = 90, hjust = 1, size = 8)
--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 #Install and load required packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(gt, webshot2, haven, labelled, dplyr, ggplot2, gtsummary, here)
+pacman::p_load(gt, webshot2, haven, labelled, dplyr, ggplot2, gtsummary, here, ggmosaic)
